b3d83e2ac4c138db92c7bb81dfa6c26fd3ad2309
Sean Kelman <seanrkelman@gmail.com>
2023-07-28 20:55:33 -0700

    added writing mazes to text files and clearing maze

--- mazemaker/GUI.java
+++ mazemaker/GUI.java
@@ -9,27 +9,52 @@ import javax.swing.event.*;
 
 public class GUI extends JFrame implements ActionListener{
     private Board board;
+    
     private int lineThickness = 2;
-    private JButton startButton, endButton, openButton, closedButton, saveButton;
-    static final char OPEN = 'o', CLOSED = 'c', START = 's', END = 'e';
-    private char mode = 'o';
+    private JButton saveButton, changeButton, clearButton;
+    private JTextField widthField, heightField;
+    private JLabel wLabel, hLabel;
     public GUI(){
         super("Maze Maker");
         JPanel topPanel = new JPanel();
         topPanel.setLayout(new FlowLayout());
-        topPanel.add(openButton = new JButton("Open"));
-        topPanel.add(closedButton = new JButton("Closed"));
-        topPanel.add(startButton = new JButton("Start"));
-        topPanel.add(endButton = new JButton("Finish"));
+        topPanel.add(wLabel = new JLabel("Width: "));
+        topPanel.add(widthField = new JTextField("Width"));
+        topPanel.add(hLabel = new JLabel("Height: "));
+        topPanel.add(heightField = new JTextField("Height"));
+        topPanel.add(changeButton = new JButton("Resize"));
+        changeButton.addActionListener(this);
         add(topPanel, BorderLayout.NORTH);
         add(board = new Board(15, 15), BorderLayout.CENTER);
+        JPanel botPanel = new JPanel();
+        botPanel.setLayout(new FlowLayout());
+        botPanel.add(clearButton = new JButton("Clear"));
+        botPanel.add(saveButton = new JButton("Save"));
+        clearButton.addActionListener(this);
+        saveButton.addActionListener(this);
+        add(botPanel, BorderLayout.SOUTH);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(750, 750);
         setVisible(true);
     }
 
     @Override
-    public void actionPerformed(ActionEvent e){}
+    public void actionPerformed(ActionEvent e){
+        Maze maze = board.getMaze();
+        if(e.getSource() == saveButton){
+            maze.writeMaze();
+            return;
+        }
+        if(e.getSource() == clearButton){
+            maze.clearMaze();
+            board.setMaze(maze);
+            board.repaint();
+            return;
+        }
+        if(e.getSource() == changeButton){
+            return;
+        }
+    }
 
 
     private class Board extends JPanel implements MouseListener{
@@ -56,26 +81,29 @@ public class GUI extends JFrame implements ActionListener{
                         case 3: color = Color.RED; break;
                     }
                     g2d.setPaint(color);
-                    g2d.fill(new Rectangle2D.Double(j*cellLength, i*cellLength, cellLength, cellLength));
+                    g2d.fill(new Rectangle2D.Double(75+ j*cellLength, i*cellLength, cellLength, cellLength));
                 }
             }
             g2d.setPaint(Color.BLACK);
             g2d.setStroke(new BasicStroke(lineThickness));
-            for(int i = 0; i < maze.getHeight()-1; i++){
-                g2d.draw(new Line2D.Double(0, (i+1) * cellLength, maze.getWidth()*cellLength, (i+1) * cellLength));
+            for(int i = 0; i < maze.getHeight()+1; i++){
+                g2d.draw(new Line2D.Double(75, (i * cellLength), 75 + (maze.getWidth()*cellLength), i * cellLength));
             }
-            for(int i = 0; i < maze.getWidth()-1; i++){
-                g2d.draw(new Line2D.Double((i+1) * cellLength, 0 , (i+1) * cellLength, maze.getWidth()*cellLength));
+            for(int i = 0; i < maze.getWidth()+1; i++){
+                g2d.draw(new Line2D.Double(75 + (i * cellLength), 0 , 75 + (i * cellLength), maze.getWidth()*cellLength));
             }
         }
 
         @Override
         public void mouseClicked(MouseEvent e) {
-            int xpos=e.getX()/cellLength;
+            int xpos=(e.getX()-75)/cellLength;
             int ypos=e.getY()/cellLength;
             if(SwingUtilities.isLeftMouseButton(e)){
                 System.out.println(xpos+", "+ypos);
                 maze.updateCell(xpos, ypos);
+            }else if(SwingUtilities.isRightMouseButton(e)){
+                System.out.println(xpos+", "+ypos);
+                maze.updateCell(xpos, ypos, -1);
             }
             repaint();
         }
@@ -87,6 +115,12 @@ public class GUI extends JFrame implements ActionListener{
         public void mouseEntered(MouseEvent e) {}
         @Override
         public void mouseExited(MouseEvent e) {}
-
+        
+        public Maze getMaze(){
+            return this.maze;
+        }
+        public void setMaze(Maze maze){
+            this.maze = maze;
+        }
     }
 }
--- mazemaker/Maze.java
+++ mazemaker/Maze.java
@@ -1,10 +1,43 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+
 public class Maze {
     private int[][] maze;
     public Maze(int width, int height){
         this.maze = new int[height][width];
     }
-    public void writeMaze(){
-        //write the maze to a csv file so it can be used in the maze solver
+
+    /**
+     * <p>
+     * exports the contents of the maze to a txt file
+     * @return True = wrote maze correctly False = something went wrong
+     */
+    public boolean writeMaze(){
+        int count = 0;
+        File myObj = new File(".\\mazes\\maze" + count + ".txt");
+        try{
+            while(!myObj.createNewFile()){
+                count++;
+                myObj = new File(".\\mazes\\maze" + count + ".txt");
+            }
+            FileWriter fw = new FileWriter(myObj);
+            fw.write(getHeight()+ "\n" + getWidth()+ "\n");
+            for(int i = 0; i < getHeight(); i++){
+                String s = "";
+                for(int j = 0; j < getWidth(); j++){
+                    s += "" + getCell(j, i)+",";
+                }
+                s+="\n";
+                fw.write(s);
+            }
+            fw.close();
+            return true;
+        }catch(IOException e){
+            System.out.println("error");
+            return false;
+        }
+        
     }
 
     /**
@@ -22,15 +55,18 @@ public class Maze {
 
     /**
      * <p>
-     * Will increment the value of the cell (cells start at 0) (0=open, 1=closed, 2=start, 3=end)
+     * Will add the passed value to the value of the cell (cells start at 0) (0=open, 1=closed, 2=start, 3=end)
      * @param x the x-value of the cell
      * @param y the y-value of the cell
-     * @param val the new value of the cell (0=open, 1=closed, 2=start, 3=end) (uses % operator to adjust values to fit this range)
+     * @param val the value to be added to the value of the cell (0=open, 1=closed, 2=start, 3=end) (uses % operator to adjust values to fit this range)
      */
     public void updateCell(int x, int y, int val){
         if(x <= maze[0].length && y<= maze.length){
             //0: open; 1: closed; 2: start; 3: end
-            maze[y][x] = val % 4;
+            maze[y][x] = (maze[y][x] + val) % 4;
+            while(maze[y][x] < 0){
+                maze[y][x] += 4;
+            }
         }
     }
 
@@ -79,12 +115,21 @@ public class Maze {
         }
     }
      /**
+      * <p>
      * Creates a new blank maze given new height and width parameters
      * @param newHeight the number of cells in the height of the new maze
      * @param newWidth the number of cells in the width of the new maze
      */
     public void updateSize(int newHeight, int newWidth){
         this.maze = new int[newHeight][newWidth];
+    }
+
+    /**
+     * <p>
+     * Clears the maze, replacing all cells with empty cells
+     */
+    public void clearMaze(){
+        this.maze = new int[getHeight()][getWidth()];
     }
 
 }
