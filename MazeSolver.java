44b522a792417ebb8ad4d298a839485a297db819
Sean Kelman <seanrkelman@gmail.com>
2023-08-01 20:40:56 -0700

    restarting

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
@@ -7,7 +9,7 @@ import java.util.Stack;
 public class MazeSolver {
     private Maze maze;
     private Stack<Integer> solution = new Stack<Integer>();
-    private int start = 0;
+    private Coordinate start;
 
     public MazeSolver(String filename){
         this.maze = new Maze(loadMaze(filename));
@@ -27,7 +29,7 @@ public class MazeSolver {
                 for(int j = 0; j < temp.length; j++){
                     arr[i][j] = Integer.parseInt(temp[j]);
                     if(arr[i][j] % 4 == 2){
-                        this.start = (i*10) + j;
+                        this.start = new Coordinate(j, i);
                     }
                 }
             }
@@ -40,69 +42,14 @@ public class MazeSolver {
         }  
     }
     public void solveMaze(){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(this.start);
+        Queue<List<Integer>> q = new LinkedList<>();
         while(q.peek() != null){
-            int i = q.peek()/10;
-            int j = q.peek()%10;
-            if(i < maze.getHeight() - 1){
-                if(maze.getCell(j, i+1) == 0){
-                    int x = ((i+1) * 10) + j;
-                    q.add(x);
-                    x = (i * 10) + j;
-                    maze.setCell(j, i+1, x+11);
-                }else if(maze.getCell(j, i+1) == 3){
-                    int x = ((i+1)*10) + j;
-                    solution.push(x);
-                    x = (i * 10) + j;
-                    maze.setCell(j, i+1, x+11);
-                }
-            }
-            if(j < maze.getWidth() -1){
-                if(maze.getCell(j+1, i) == 0){
-                    int x = (i * 10) + j + 1;
-                    q.add(x);
-                    x = (i * 10) + j;
-                    maze.setCell(j+1, i, x+11);
-                }else if(maze.getCell(j + 1, i) == 3){
-                    int x = (i*10) + j + 1;
-                    solution.push(x);
-                    x = (i * 10) + j;
-                    maze.setCell(j + 1, i, x+11);
-                }
-            }
-            if(i > 0){
-                if(maze.getCell(j, i-1) == 0){
-                    int x = ((i-1)*10) + j;
-                    q.add(x);
-                    x = (i * 10) + j;
-                    maze.setCell(j, i-1, x+11);
-                }else if(maze.getCell(j, i-1) == 3){
-                    int x = ((i-1)*10) + j;
-                    solution.push(x);
-                    x = (i * 10) + j;
-                    maze.setCell(j, i-1, x+11);
-                }
-            }
-            if(j > 0){
-                if(maze.getCell(j-1, i) == 0){
-                    int x = (i * 10) + j - 1;
-                    q.add(x);
-                    x = (i * 10) + j;
-                    maze.setCell(j-1, i, x+11);
-                }else if(maze.getCell(j - 1, i) == 3){
-                    int x = (i*10) + j - 1;
-                    solution.push(x);
-                    x = (i * 10) + j;
-                    maze.setCell(j-1, i, x+11);
-                }
-            }
-            q.remove();
-            //System.out.println(q);
+            
 
         }
+        maze.writeMaze();
         while(solution.peek() != null && solution.peek() != 2){
-            int x = solution.peek() - 11;
+            int x = solution.peek();
             int j = x % 10;
             int i = x / 10;
             System.out.println(solution);
@@ -110,5 +57,4 @@ public class MazeSolver {
         }
         System.out.println(solution);
     }   
-
 }
