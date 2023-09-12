8b1dd8acc1eb5fc7758bb06e21d0086a8fe21fa2
Sean Kelman <seanrkelman@gmail.com>
2023-09-12 08:06:22 -0700

    first iteration djikstras

--- DjikstraSolver.java
+++ DjikstraSolver.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -26,6 +28,7 @@ public class DjikstraSolver extends MazeSolver{
         ArrayList<String> solution = new ArrayList<String>();
         int currCost = 0;
         String s = "";
+        File f = new File(".\\solutions\\solution0.txt");
         Queue<Cell> q = new LinkedList<Cell>();
         vertices[start.getY()][start.getX()] = currCost;
         q.add(start);
@@ -64,7 +67,55 @@ public class DjikstraSolver extends MazeSolver{
         }
         while(vertices[path.peek().getY()][path.peek().getX()] != 0){
             int MinCost = Integer.MAX_VALUE;
-            
+            int x = path.peek().getX();
+            int y = path.peek().getY();
+            int minx = -1;
+            int miny = -1;
+            if(x > 0 && (vertices[y][x-1] != -1 && vertices[y][x-1] < MinCost)){
+                MinCost = vertices[y][x-1];
+                minx = x-1;
+                miny = y;
+            }
+            if (y > 0 && (vertices[y-1][x] != -1 && vertices[y-1][x] < MinCost)){
+                MinCost = vertices[y-1][x];
+                minx = x;
+                miny = y-1;
+            }
+            if (x < maze.getWidth() - 1 && (vertices[y][x+1] != -1 && vertices[y][x+1] < MinCost)){
+                MinCost = vertices[y][x+1];
+                minx = x+1;
+                miny = y;
+            }
+            if(y < maze.getHeight() -1 && (vertices[y+1][x] != -1 && vertices[y+1][x] < MinCost)){
+                MinCost = vertices[y+1][x];
+                minx = x;
+                miny = y+1;
+            }
+            path.push(maze.getCell(minx, miny));
+            if(MinCost == Integer.MAX_VALUE){
+                System.out.println("No path found");
+                return null;
+            }
+            else if(MinCost == 0){
+                break;
+            }
+        }
+        while(!path.isEmpty()){
+            Cell curr = path.pop();
+            solution.add("solving " + curr);
+            s += "solving " + curr + "\n";
+        }
+        try{
+            int count = 0;
+            while(!f.createNewFile()){
+                count++;
+                f = new File(".\\solutions\\solution" + count + ".txt");
+            }
+            FileWriter fw = new FileWriter(f);
+            fw.write(s);
+            fw.close();
+        }catch(Exception e){
+            System.out.println("error writing to file");
         }
         return solution.toArray(new String[solution.size()]);
     }
