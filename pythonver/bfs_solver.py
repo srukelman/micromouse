96cdc0b37a935f0d9570014e4ae0b5fe8d77d6a0
Sean Kelman <seanrkelman@gmail.com>
2023-12-31 13:01:06 -0800

    made solver

---
+++ pythonver/bfs_solver.py
@@ -0,0 +1,60 @@
+from typing import Any
+from maze import Maze
+from cell import Cell
+from collections import deque
+
+class BFS_Solver:
+
+    def __init__(self, maze : Maze):
+        self._maze = maze
+        self._start = maze.get_start()
+        self._finish = None
+        self._path = []
+        self._steps = []
+
+    def solve(self) -> (list[Cell], list[str]):
+        q = deque()
+        sol = []
+        q.add(self._start)
+        sol.append('added ' + str(self._start))
+        while q:
+            curr = q.popleft()
+            if curr.get_value() == 3:
+                self._finish = curr
+                break
+            elif not curr.get_visited():
+                curr.visit()
+                sol.append('visited ' + str(curr))
+                self._steps.append(curr)
+                for cell in self._get_neighbors(curr):
+                    sol.append('added ' + str(cell))
+                    q.append(cell)
+        
+        if not self._finish:
+            return None
+        else:
+            self._path = self._get_path()
+            return self._path, sol
+        
+    def _get_path(self) -> list[Cell]:
+        path = []
+        curr = self._finish
+        while curr:
+            path.append(curr)
+            curr = curr.get_prev_cell()
+        return path[::-1]
+
+    def _get_neighbors(self, cell : Cell) -> list[Cell]:
+        x = cell.get_x()
+        y = cell.get_y()
+        neighbors = []
+        if x > 0:
+            neighbors.append(self._maze.get_cell(x-1, y))
+        if x < self._maze.get_width() - 1:
+            neighbors.append(self._maze.get_cell(x+1, y))
+        if y > 0:
+            neighbors.append(self._maze.get_cell(x, y-1))
+        if y < self._maze.get_height() - 1:
+            neighbors.append(self._maze.get_cell(x, y+1))
+        return neighbors
+
--- pythonver/gui.py
+++ pythonver/gui.py
@@ -2,11 +2,26 @@ import pygame
 from cell import Cell
 from maze import Maze
 
+COLORS = {
+    1: (0, 0, 0), # black - wall
+    0: (255, 255, 255), # white - empty
+    2: (0, 255, 0), # green - start
+    3: (255, 0, 0), # red - finish
+    4: (0, 0, 255), # blue - path
+    5: (255, 255, 0), # yellow - visited
+    6: (255, 0, 255), # purple - added (not visited)
+}
+    
+
 class View:
+
     def __init__(self, maze: list[list[Cell]]) -> None:
         self._maze = Maze(maze)
         self._running = True
-    
+        self._solution = None
+        self._index = 0
+        self._solving = False
+        
     def run(self) -> None:
         pygame.init()
         X , Y = 800, 800
@@ -14,8 +29,11 @@ class View:
         self._draw()
         pygame.display.set_caption('Maze Solver')
         clock = pygame.time.Clock()
-        while self._running:
-            clock.tick(10)
+        while self._running and self._index < len(self._solution):
+            clock.tick(1)
+            self._handle_events()
+            if self._solving:
+                _update_maze(self._solution[self._index])
         pygame.quit()
 
     def _handle_events(self):
@@ -25,12 +43,12 @@ class View:
             elif event.type == pygame.VIDEORESIZE:
                 self._resize_surface(event.size)
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    return self._board.update_faller('<')
-                elif event.key == pygame.K_RIGHT:
-                    return self._board.update_faller('>')
-                elif event.key == pygame.K_SPACE:
-                    self._board.update_faller('R')
+                if event.key == pygame.K_SPACE:
+                    self._index = 0
+                    self._solving = True
+                elif event.key == pygame.K_RETURN:
+                    self._solving = False
+
 
     def _draw(self) -> None:
         self._screen.fill((150, 150, 150)) # gray background color
@@ -43,13 +61,23 @@ class View:
             for cell in row:
                 self._draw_cell(cell)
 
+    def _resize_surface(self, size: tuple[int, int]) -> None:
+        pygame.display.set_mode(size, pygame.RESIZABLE)
+
+    def _update_maze(self, cell: Cell) -> None:
+        self._draw_cell(cell)
+        pygame.display.flip()
+        self._index += 1
+
     def _draw_cell(self, cell: Cell) -> None:
         left_margin = self._screen.get_width() // 5
         top_margin = self._screen.get_height() // 5
-        cell_width = self._screen.get_width() // int(self._maze.get_width() * .9)
-        cell_height = self._screen.get_height() // int(self._maze.get_height() * .9)
+        cell_width = int(self._screen.get_width() // self._maze.get_width() * .6)
+        cell_height = int(self._screen.get_height() // self._maze.get_height() * .6)
         x = cell.get_x() * cell_width + left_margin
         y = cell.get_y() * cell_height + top_margin
 
-        pygame.draw.rect(self._screen, (0, 0, 0), (x, y, cell_width, cell_height), 1)
+        color = COLORS[cell.get_value()]
+
+        pygame.draw.rect(self._screen, color, (x, y, cell_width, cell_height), 100)
 
